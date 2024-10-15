fb88e5bf8ed2ac92c665f79e130550a6b34b915e
biranjay kumar gupta <biranjaykumargupta@gmail.com>
2024-10-15 17:51:47 +0530

    added the cors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { connectDb } from "./config/dbConenction.js";
+import cors from "cors"
 // *********** All-Routes *************
 import auth from "./routes/auth.routes.js";
 import user from "./routes/user.routes.js";
@@ -7,6 +8,15 @@ import user from "./routes/user.routes.js";
 
 import cookieParser from "cookie-parser";
 const app = express();
+// Use cors middleware
+app.use(cors());
+
+app.use(
+  cors({
+    origin: "*", // Replace with the frontend's URL (React app)
+    methods: "GET,POST,PUT,DELETE,PATCH", // Allowed methods
+  })
+);
 
 //middle wares
 app.use(express.json());
