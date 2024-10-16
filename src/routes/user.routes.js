68c48143f39c48f493c43b47499870ebf229f52e
biranjay kumar gupta <biranjaykumargupta@gmail.com>
2024-10-16 18:35:09 +0530

    added the user-into APIs

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { claimPoints, getAllUser, getMonthlyData, getTodayHistory, getUserHistory, getWeeklyData } from "../controllers/user.controller.js";
+import { claimPoints, getAllUser, getMonthlyData, getTodayHistory, getUserHistory, getUserWithHelpOfId, getUserWithHelpOfToken, getWeeklyData } from "../controllers/user.controller.js";
+import verifyToken from "../middlewares/verifyToken.js";
 
 const router = express.Router();
 
 router.get("/get-users", getAllUser);
+router.post("/get-users-info", verifyToken, getUserWithHelpOfToken);
+router.post("/get-users-info", verifyToken, getUserWithHelpOfId);
 router.patch("/claim-points", claimPoints);
 router.get("/your-daily-history", getTodayHistory);
 router.get("/your-weekly-history", getWeeklyData);
